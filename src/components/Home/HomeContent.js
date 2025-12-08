5b17269453480ac345cd4e837d5e5094853b8d5d
abhisheks28 <abhishekmys03@gmail.com>
2025-12-08 11:50:27 +0530

    Test flow changes (#7)
    Co-authored-by: kris22krishna <kris22krishna@gmail.com>

--- src/components/Home/HomeContent.js
+++ src/components/Home/HomeContent.js
@@ -4,7 +4,7 @@ import Styles from "../../app/page.module.css";
 import { Button, CircularProgress } from "@mui/material";
 import { Award, Clock, Contact, ArrowRight, Target } from "lucide-react";
 import dynamic from "next/dynamic";
-const LandingIllustration = dynamic(() => import("@/components/LottieAnimations/Landing/Landing.component"), { ssr: false });
+// const LandingIllustration = dynamic(() => import("@/components/LottieAnimations/Landing/Landing.component"), { ssr: false });
 import SampleDashboard from "@/components/SampleDashboard/SampleDashboard.component";
 import Footer from "@/components/Footer/Footer.component";
 import { useRouter } from "next/navigation";
@@ -153,7 +153,7 @@ const HomeContent = () => {
                     </div>
                     <h1>Math Skills Proficiency Test</h1>
                     <p className={Styles.subtitle}>Discover Your Math Mastery Level</p>
-                    <div className={Styles.badgesContainer}>
+                    {/* <div className={Styles.badgesContainer}>
                         <div className={Styles.badge}>
                             <Target className={Styles.badgeIcon} />
                             <span>Assess</span>
@@ -166,7 +166,7 @@ const HomeContent = () => {
                             <Clock className={Styles.badgeIcon} />
                             <span>Celebrate Math Skills</span>
                         </div>
-                    </div>
+                    </div> */}
 
                     <div style={{ marginTop: '32px' }}>
                         <Button
@@ -190,7 +190,7 @@ const HomeContent = () => {
                         </Button>
                     </div>
 
-                    <div className={Styles.featuresContainer}>
+                    {/* <div className={Styles.featuresContainer}>
                         <div className={Styles.featureItem}>
                             <Clock />
                             <p>30 Minutes</p>
@@ -205,10 +205,15 @@ const HomeContent = () => {
                             <Award />
                             <p>Instant Result</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className={Styles.illustrationContainer}>
-                    <LandingIllustration />
+                    {/* <LandingIllustration /> */}
+                    <img
+                        src="/HeroIllustration.gif"
+                        alt="Landing Illustration"
+                        style={{ width: '90%', height: 'auto' }}
+                    />
                 </div>
             </div>
 
